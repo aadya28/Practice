e58dcea147a2fe6bee5832c2702db1c7fc15a4bb
Aadya Srivastava <aadya28.srivastava@gmail.com>
2023-04-09 14:08:02 +0530

    Initial Commit

---
+++ WordPlay.java
@@ -0,0 +1,9 @@
+
+public class WordPlay {
+    public static void isVowel(Character ch) {
+    System.out.println(ch);
+    }
+    public static void main(String[] args) {
+    isVowel('a');
+    }
+}
