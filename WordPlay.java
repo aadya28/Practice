34b1b8c1bf8d7ee9235f5e87d5227cac7e933d3b
Aadya Srivastava <aadya28.srivastava@gmail.com>
2023-04-10 19:08:56 +0530

    Program to check if the input character is vowel or not

--- WordPlay.java
+++ WordPlay.java
@@ -1,9 +1,23 @@
+//Program to check if the input character is vowel or not
+import java.util.Scanner;
 
 public class WordPlay {
-    public static void isVowel(Character ch) {
-    System.out.println(ch);
+    public static void isVowel(Character c) {
+        Character[] charArray = {'a','e','i','o','u','A','E','O','U','I'};
+        boolean contains = false; //contains() only works on list
+        for (char ch : charArray) {
+            if (ch == c) {
+                contains = true;
+                break;
+            }
+        }
+        System.out.println(contains);
     }
+
     public static void main(String[] args) {
-    isVowel('a');
+        try (Scanner sc = new Scanner(System.in)) {
+            char c = sc.next().charAt(0); //There is no nextChar() in Java
+            isVowel(c);
+        }
     }
 }
