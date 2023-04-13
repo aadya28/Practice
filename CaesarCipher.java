855ec07a4c4f52b4e42f69517dc9f5294d88cd72
Aadya Srivastava <aadya28.srivastava@gmail.com>
2023-04-13 19:09:57 +0530

    Program to encrypt a string using Caesar Cipher for both upper and lowercase letters

--- CaesarCipher.java
+++ CaesarCipher.java
@@ -3,20 +3,33 @@ import java.util.Scanner;
 
 public class CaesarCipher {
     /* This method returns a String that has been encrypted using the Caesar Cipher 
-    For Example - Sample Input: encrypt(“FIRST LEGION ATTACK EAST FLANK!”, 23)
-    Sample Output : “CFOPQ IBDFLK XQQXZH BXPQ CIXKH!” */
+    For Example - Sample Input: encrypt(“First Legion”, 17) 
+    Sample Output : “Wzijk Cvxzfe” */
     
     public static void encrypt(String input , int key){
         String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         String shiftedAlphabet = alphabet.substring(key)+alphabet.substring(0,key);
         int length = input.length();
         String newstr = "";
+        Boolean lc = false;
         for (int i = 0; i < length;i++){
-            char toReplace = input.charAt(i);
+            Character toReplace = input.charAt(i);
+            if(Character.isLowerCase(toReplace)){
+                toReplace = Character.toUpperCase(toReplace);
+                lc = true;
+            }
             int idx  = alphabet.indexOf(toReplace);
             if (idx != -1){
-                char replaceWith = shiftedAlphabet.charAt(idx);
-                newstr = newstr + replaceWith;
+                if (lc){
+                    Character replaceWith = shiftedAlphabet.charAt(idx);
+                    replaceWith = Character.toLowerCase(replaceWith);
+                    newstr = newstr + replaceWith;
+                    lc = false;
+                }
+                else{
+                    char replaceWith = shiftedAlphabet.charAt(idx);
+                    newstr = newstr + replaceWith;
+                }  
             }
             else{
                 newstr = newstr + input.charAt(i);
