2194b473f58c8e246adc03c7d9e9bb89e811fc32
Aadya Srivastava <aadya28.srivastava@gmail.com>
2023-04-22 21:29:10 +0530

    Program to read text from a file and create methods to access individual words and lines.

---
+++ ReadFile.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+
+public class ReadFile{
+    public static void getWords() throws IOException{
+        List<String> strgfile = new ArrayList<String>();
+        try (Scanner scan = new Scanner(new FileReader("/Users/aadya/HackerRank/common.txt"))) {
+            while(scan.hasNext()){
+            String word = scan.next();
+            strgfile.add(word);
+            }
+        }
+        String[] common = strgfile.toArray(new String[0]);
+        for (String eachString : common){
+            System.out.println(eachString);
+        }
+	}
+
+    public static void getLines() throws IOException{
+        try (Scanner scan = new Scanner(new FileReader("/Users/aadya/HackerRank/common.txt"))) {
+            while(scan.hasNextLine()){
+            String Line = scan.nextLine();
+            System.out.println(Line);
+            }
+        }
+    }
+    public static void main(String[] args) throws FileNotFoundException {
+        try {
+            getWords();
+            getLines();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
